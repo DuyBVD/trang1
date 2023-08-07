e7ba70ff2f6d85adb53174f840edd081eb521d1c
DuyBVD <duyk10.261@eaut.edu.vn>
2023-08-07 21:41:30 +0700

    login

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="box">
+      <p>ĐĂNG KÍ TÀI KHOẢN</p>
+        <div class="box1">
+          <h1>TÊN ĐĂNG NHẬP:</h1>
+          <input type="text" id="username" name="username" placeholder="Tên đăng nhập"></input>
+        </div>
+        <div class="box2">
+          <h2>MẬT KHẨU:</h2>  
+          <input type="password" id="pwd" name="pwd" placeholder="Mật khẩu"></input>
+        </div>
+        <div class="box3">
+          <h3>NHẬP LẠI MẬT KHẨU:</h3>  
+          <input type="password" id="pwd" name="pwd" placeholder="Nhập lại mật khẩu"></input>
+        </div>
+        <div class="box4">
+          <h4>HỌ TÊN:</h4>
+          <input type="text" id="username" name="username" placeholder="Họ tên"></input>
+        </div>
+        <div class="box5">
+          <h5>EMAIL:</h5>
+          <input type="text" id="email" name="email" placeholder="Nhập email"></input>
+        </div>
+        <div class="box6">
+          <h6>SỐ ĐIỆN THOẠI:</h6>  
+          <input type="number" id="sdt" name="sdt" placeholder="Nhập số điện thoại "></input>
+        </div>
+        <hr></hr>
+        <button class="Done">GỬI</button>
+             
     </div>
+ 
   );
 }
 
